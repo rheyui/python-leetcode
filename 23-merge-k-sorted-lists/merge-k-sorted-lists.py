27a0fb31f2cd339ff8263d290fa272d4a313b4e4
rheyui <107768071+rheyui@users.noreply.github.com>
2026-06-04 16:30:17 +0530

    Time: 17 ms (59.35%) | Memory: 18.2 MB (49.59%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -0,0 +1,31 @@
+import heapq
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
+        """
+
+        heap = []
+
+        # Put first node of each list into heap
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        dummy = ListNode(0)
+        curr = dummy
+
+        while heap:
+
+            val, i, node = heapq.heappop(heap)
+
+            curr.next = node
+            curr = curr.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
+        
