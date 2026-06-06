b3f74195a22fd8700911415c79c3ded9664a08b0
rheyui <107768071+rheyui@users.noreply.github.com>
2026-06-06 17:24:37 +0530

    Time: 28 ms (69.76%) | Memory: 13.4 MB (26.44%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -1,12 +1,7 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
 class Solution(object):
     def findSubstring(self, s, words):
-        """
-        :type s: str
-        :type words: List[str]
-        :rtype: List[int]
-        """
 
         if not s or not words:
             return []
@@ -15,46 +10,55 @@ class Solution(object):
         total_words = len(words)
         total_len = word_len * total_words
 
-        word_map = Counter(words)
-        ans = []
+        word_count = Counter(words)
 
-        # Try all possible starting positions
+        result = []
+
+        # Try every possible starting offset
         for i in range(word_len):
 
             left = i
-            curr_map = Counter()
             count = 0
 
+            seen = defaultdict(int)
+
+            # Move window in steps of word_len
             for right in range(i, len(s) - word_len + 1, word_len):
 
                 word = s[right:right + word_len]
 
                 # Valid word
-                if word in word_map:
-                    curr_map[word] += 1
+                if word in word_count:
+
+                    seen[word] += 1
                     count += 1
 
-                    # Remove extra occurrences
-                    while curr_map[word] > word_map[word]:
+                    # Too many occurrences
+                    while seen[word] > word_count[word]:
+
                         left_word = s[left:left + word_len]
-                        curr_map[left_word] -= 1
+
+                        seen[left_word] -= 1
                         count -= 1
+
                         left += word_len
 
-                    # Found valid substring
+                    # Found valid window
                     if count == total_words:
-                        ans.append(left)
+
+                        result.append(left)
 
                         left_word = s[left:left + word_len]
-                        curr_map[left_word] -= 1
+
+                        seen[left_word] -= 1
                         count -= 1
+
                         left += word_len
 
                 else:
                     # Reset window
-                    curr_map.clear()
+                    seen.clear()
                     count = 0
                     left = right + word_len
 
-        return ans
-        
+        return result
