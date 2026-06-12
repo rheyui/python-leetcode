c58a4651e57928dfcfb1e3bcc2a44bd968bee5b6
rheyui <107768071+rheyui@users.noreply.github.com>
2026-06-12 08:35:24 +0530

    Time: 51 ms (48.83%) | Memory: 22.5 MB (80.00%) - LeetSync

---
+++ 72-edit-distance/edit-distance.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+        m = len(word1)
+        n = len(word2)
+
+        dp = [[0]*(n+1) for _ in range(m+1)]
+
+        # base cases
+        for i in range(m+1):
+            dp[i][n] = m - i
+
+        for j in range(n+1):
+            dp[m][j] = n - j
+
+        # fill table
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = 1 + min(
+                        dp[i+1][j+1],  # replace
+                        dp[i+1][j],    # delete
+                        dp[i][j+1]     # insert
+                    )
+
+        return dp[0][0]
