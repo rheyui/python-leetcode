173b7b9af4094a6f6d53ea0e5a8ceb7f9c6724e2
rheyui <107768071+rheyui@users.noreply.github.com>
2026-06-17 16:57:34 +0530

    Time: 1072 ms (29.91%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -4,4 +4,4 @@ SELECT
     ROUND(COUNT(user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
 FROM Register
 GROUP BY contest_id
-ORDER BY percentage DESC, contest_id ASC;
+ORDER BY percentage DESC, contest_id ASC; 
