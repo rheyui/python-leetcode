4f9178314176a1ecae2b2c74014b1f413cf80745
rheyui <107768071+rheyui@users.noreply.github.com>
2026-06-18 17:46:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.93%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,6 +1,14 @@
-class Solution(object):
-    def plusOne(self, digits):
-        num = int("".join(map(str, digits)))
-        num += 1
-        
-        return list(map(int, str(num)))
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            digits[i] = 0
+
+        return [1] + digits
