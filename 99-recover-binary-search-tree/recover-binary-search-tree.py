a8e211a3e5220ebb7064f58d559a5758a7b7de62
rheyui <107768071+rheyui@users.noreply.github.com>
2026-06-28 19:01:50 +0530

    Time: 9 ms (26.98%) | Memory: 12.7 MB (51.47%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def recoverTree(self, root):
+        self.first = None
+        self.second = None
+        self.prev = None
+
+        def inorder(node):
+            if not node:
+                return
+
+            inorder(node.left)
+
+            # Check if BST property is violated
+            if self.prev and self.prev.val > node.val:
+
+                # First violation
+                if not self.first:
+                    self.first = self.prev
+
+                # Update second every time
+                self.second = node
+
+            self.prev = node
+
+            inorder(node.right)
+
+        inorder(root)
+
+        # Swap the incorrect nodes
+        self.first.val, self.second.val = self.second.val, self.first.val
+        
